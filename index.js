3790586b8d9c054bc583ca31158e1792f9b709ac
gms08 <gms@thesousas.com>
2018-05-04 11:21:07 -0500

    update yargs in index.js

--- index.js
+++ index.js
@@ -1,21 +1,17 @@
 const request = require('request');
-
+const argv = require('yargs').argv;
 
 let apiKey = '5d7eba6323b944872625fff10054842b';
-let city = 'minneapolis';
-let url = `http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=imperial`
-
-
+let city = argv.c || 'minneapolis';
+let url = `http://api.openweathermap.org/data/2.5/weather?q=${city}&units=imperial&appid=${apiKey}`;
 
 request(url, function (err, response, body) {
     if (err) {
         console.log('error:', error);
     } else {
-        // console.log('body:', body);
         let weather = JSON.parse(body)
         let message = `It's ${weather.main.temp} degrees in ${weather.name}!`;
         console.log(message);
     }
 
 });
-
